32a53dd0e17f00f78fe08e6301a7da06f2003cd7
Nageshbabu <nageshm@sea-nageshm-m.ds.ad.adp.com>
2017-01-27 14:01:20 -0800

    Added a example of grid layout.

---
+++ lessGridExample/gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+    //configure the init for project
+    grunt.initConfig({
+        //basic setting about the plugin
+        pkg: grunt.file.readJSON('package.json'),
+        //name of plugin (plugin name without grunt-contrib-)
+        watch: {
+            less: {
+                files: ['assets/less/*.less'],
+                tasks: ['less']
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ['assets/css']
+                },
+                files: {
+                    'assets/css/style.css': 'assets/less/style.less'
+                }
+            }
+        }
+    });
+
+    //load plugin
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    //load task
+    grunt.registerTask("default", ['watch']);
+};
