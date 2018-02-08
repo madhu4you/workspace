3b68a16f149a459f64cc772a01208aa59f3fbeb5
Nageshbabu, Madhu <Madhu.Nageshbabu@cdk.com>
2018-02-08 12:51:18 -0800

    Javascript programs

---
+++ JavaScript/remove-vowels.js
@@ -0,0 +1,14 @@
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
+  }
+  
+  //////////////////////////////
+  
+  function disemvowel(str) {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    
+    return str.split('').filter(function(el) {
+      return vowels.indexOf(el.toLowerCase()) == -1;
+    }).join('');
+  }
+  
