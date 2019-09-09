7f784f6f5cedf9514e0bca454a06c884d4ac271b
Nageshbabu, Madhu <Madhu.Nageshbabu@cdk.com>
2019-09-09 16:25:19 -0700

    Moved to problem solving

--- JavaScript/js/reverse-numbers.js
+++
@@ -1,7 +0,0 @@
-var reverseNumber = function(num) {
-    if(isNaN(num)) return false;
-    num = num.toString().split('');
-    num = num.reverse();
-    num = num.join('');
-    return Number(num);
-}
--- JavaScript/js/string-unique.js
+++
@@ -1,7 +0,0 @@
-//var str= "f0ac,f1e9,f16e,f122,f106,f107,f105,f104,f231,f107,f0ac,f1e9,f16e,f122,f106,f105,f104,f231,f0ac,f1e9,f16e,f122,f106,f107,f105,f104,f10d";
-function stringUnique(str) {
-    var unique = str.split(",").filter(function (x, i, a) { 
-        return a.indexOf(x) == i; 
-    });
-    return unique.join(", ");
-}
